f0d4a171edaa62a97186bd2b9f2934b7455f9ee7
Nikola Kuzmic <nikola.kuzmic945@gmail.com>
2022-02-11 22:39:19 -0500

    simplified to only 6 images

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue-icons.esm'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 new Vue({
